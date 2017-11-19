04f7434dcae34238783ecb662ffec641d2c86e90
Leroy <leroy@Leroys-Mac-mini.local>
2017-11-19 17:06:01 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 2f;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D> ();
+    }
+
+	float horizontalDirection;
+	float verticalDirection;
+    float angle;
+
+    void FixedUpdate()
+    {
+        horizontalDirection = (Input.GetKey("a")? -1f : 0f) + (Input.GetKey("d") ? 1f : 0f);
+        verticalDirection = (Input.GetKey("w")? 1f : 0f) + (Input.GetKey("s") ? -1f : 0f);
+
+		if (horizontalDirection != 0f || verticalDirection != 0f) {
+			body.velocity = (Vector2.right * horizontalDirection + Vector2.up * verticalDirection) * speed;
+
+            float angle = Mathf.Atan2(verticalDirection, horizontalDirection) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        } else {
+            body.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/World/WorldGenerator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class WorldGenerator : MonoBehaviour
+{
+    public Sprite[] Sprites;
+
+    public int worldWidth = 20;
+    public int worldHeight = 20;
+
+    private Grid tileGrid;
+    private Tilemap tilemap;
+
+    void Awake()
+    {
+        tileGrid = GetComponentInParent<Grid>();
+        tilemap = GetComponent<Tilemap>();
+
+
+
+        Sprite[,] tiles = Generate();
+
+        InstantiateWorld(tiles);
+    }
+
+    private Sprite[,] Generate()
+    {
+        Sprite[,] tiles = new Sprite[worldWidth, worldHeight];
+
+        for (int x = 0; x < worldWidth; x++)
+        {
+            for (int y = 0; y < worldHeight; y++)
+            {
+                tiles[x, y] = Sprites[Random.Range(0, Sprites.Length)];
+            }
+        }
+
+        return tiles;
+    }
+
+    private void InstantiateWorld(Sprite[,] tiles)
+    {
+        for (int x = 0; x < worldWidth; x++)
+        {
+            for (int y = 0; y < worldHeight; y++)
+            {
+                Tile tile = ScriptableObject.CreateInstance<Tile>();
+                tile.sprite = tiles[x, y];
+
+                tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+
+            }
+        }
+    }
+}
